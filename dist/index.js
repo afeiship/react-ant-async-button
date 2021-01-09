7c5c036eaa105fe6b8173b74641bce308c1ebfef
afeiship <1290657123@qq.com>
2021-01-09 16:46:14 +0800

    Release 1.0.2

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@
  * name: @jswork/next-content-type
  * description: Get correct content type for next.
  * homepage: https://github.com/afeiship/next-content-type
- * version: 1.0.1
- * date: 2021-01-06 21:43:25
+ * version: 1.0.2
+ * date: 2021-01-09 16:46:08
  * license: MIT
  */
 
